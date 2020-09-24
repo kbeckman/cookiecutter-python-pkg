34f3889ae113fb637df66d479580f0d0aae48235
kbeckman <kbeckman@redfournine.com>
2020-09-24 11:06:42 -0500

    adding package_slug/src/hello_world content

---
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_slug}}/__init__.py
@@ -0,0 +1,3 @@
+from .hello_world import *
+
+__version__ = '0.0.1'
---
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_slug}}/hello_world.py
@@ -0,0 +1,11 @@
+# pylint: disable=no-self-use
+
+class HelloWorld:
+    """Generic demo 'hello world' class."""
+    def echo_hello(self):
+        """Generic demo 'hello, world' class method.
+
+        returns:
+            [string] hello, world!
+        """
+        return 'hello, world!'
